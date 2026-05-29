f25b71b2de85854fc9006ac92dc7d80c9598b1b3
Amirali Hossaini <amirdev1024@gmail.com>
2026-05-29 17:55:42 +0330

    chore: install laravel

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    protected static ?string $password;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => $this->generateValidEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+
+    private function generateValidEmail(): string
+    {
+        $username = fake()->unique()->userName();
+        $domain = fake()->randomElement([
+            'gmail.com',
+            'yahoo.com',
+            'outlook.com',
+            'hotmail.com',
+        ]);
+
+        return $username.'@'.$domain;
+    }
+}
