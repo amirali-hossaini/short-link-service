215dd5f88de8d80a535fe3508441ba45229be738
Amirali Hossaini <amirdev1024@gmail.com>
2026-05-30 02:03:29 +0330

    refactor(user): use toISOString for email verification date

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -13,7 +13,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
+            'email_verified_at' => $this->email_verified_at?->toISOString(),
         ];
     }
 }
